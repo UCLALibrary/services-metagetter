a7ba7f1ab9e90c90038f58e9d2ba5783cda16830
Kevin S. Clarke <ksclarke@ksclarke.io>
2022-04-22 13:37:06 -0400

    [SERV-428] Update dependencies and parent project

--- src/main/java/edu/ucla/library/services/metadata/MetadataSetter.java
+++ src/main/java/edu/ucla/library/services/metadata/MetadataSetter.java
@@ -1,10 +1,11 @@
 
 package edu.ucla.library.services.metadata;
 
+import static info.freelibrary.util.Constants.EMPTY;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -17,6 +18,7 @@ import java.util.concurrent.Callable;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvException;
 
 import info.freelibrary.util.I18nRuntimeException;
 import info.freelibrary.util.Logger;
@@ -33,8 +35,12 @@ import picocli.CommandLine.Parameters;
 /**
  * An application that adds A/V metadata values to a CSV file.
  */
+@SuppressWarnings("PMD.SystemPrintln")
 public final class MetadataSetter implements Callable<Integer> {
 
+    /**
+     * Logger for the MetadataSetter.
+     */
     private static final Logger LOGGER = LoggerFactory.getLogger(MetadataSetter.class, MessageCodes.BUNDLE);
 
     /**
@@ -93,12 +99,10 @@ public final class MetadataSetter implements Callable<Integer> {
      */
     @Override
     public Integer call() {
-        if (!fileDirExists(Arrays.asList(myCsvPath))) {
+        if (!fileDirExists(Arrays.asList(myCsvPath)) || !fileDirExists(myMediaPath)) {
             return ExitCodes.FILE_DOESNT_EXIST;
         }
-        if (!fileDirExists(myMediaPath)) {
-            return ExitCodes.FILE_DOESNT_EXIST;
-        }
+
         if (!validFFProbe(myFfmpegPath)) {
             return ExitCodes.PROBE_DOESNT_EXIST;
         }
@@ -136,6 +140,7 @@ public final class MetadataSetter implements Callable<Integer> {
      */
     public boolean fileDirExists(final List<String> aFilePathList) {
         final StringBuilder stringBuilder = new StringBuilder();
+        final boolean result;
         final int length;
 
         for (final String filePath : aFilePathList) {
@@ -145,11 +150,13 @@ public final class MetadataSetter implements Callable<Integer> {
         }
 
         if ((length = stringBuilder.length()) == 0) {
-            return true;
+            result = true;
         } else {
             System.err.println(stringBuilder.delete(length - System.lineSeparator().length(), length).toString());
-            return false;
+            result = false;
         }
+
+        return result;
     }
 
     /**
@@ -175,11 +182,11 @@ public final class MetadataSetter implements Callable<Integer> {
      * @param aPath Path to file to be read and copied/updated
      * @throws I18nRuntimeException A wrapped exception thrown while adding A/V metadata
      */
-    private void addMetaToCsv(final Path aPath) throws I18nRuntimeException {
+    private void addMetaToCsv(final Path aPath) {
         final File outputFile = Paths.get(myOutputPath, aPath.toFile().getName()).toFile();
 
-        try (CSVReader reader = new CSVReader(new FileReader(aPath.toFile()));
-                CSVWriter writer = new CSVWriter(new FileWriter(outputFile))) {
+        try (CSVReader reader = new CSVReader(Files.newBufferedReader(aPath));
+                CSVWriter writer = new CSVWriter(Files.newBufferedWriter(outputFile.toPath(), UTF_8))) {
             final List<String[]> input = reader.readAll();
             final List<String[]> output = new ArrayList<>(input.size());
             final boolean hasAllMetas = allMetaFieldsPresent(input.get(0));
@@ -204,7 +211,7 @@ public final class MetadataSetter implements Callable<Integer> {
             }
         } catch (final IOException details) { // Catches FileNotFoundException(s) and other IOException(s), too
             throw new I18nRuntimeException(details, MessageCodes.BUNDLE, MessageCodes.MG_104, details.getMessage());
-        } catch (final FileFormatException details) {
+        } catch (final FileFormatException | CsvException details) {
             throw new I18nRuntimeException(details, MessageCodes.BUNDLE, MessageCodes.MG_000, details.getMessage());
         }
     }
@@ -230,11 +237,13 @@ public final class MetadataSetter implements Callable<Integer> {
      * Method to copy/modify data rows from source file.
      *
      * @param aSource The original row from the source file
+     * @param aHasColumns An indication that the CSV has column names
      * @return The modified CSV row
      * @throws FileNotFoundException If a media file could not be found
      * @throws FfProbeException If FFProbe encounters an error while reading the media file
      * @throws FileFormatException If the media file doesn't have a file extension
      */
+    @SuppressWarnings("PMD.CollapsibleIfStatements")
     private String[] buildARow(final boolean aHasColumns, final String... aSource)
             throws FileNotFoundException, FileFormatException {
         final int fileColumnIndex = myCsvHeaders.getFileNameIndex();
@@ -242,7 +251,7 @@ public final class MetadataSetter implements Callable<Integer> {
         final String fileName = line[fileColumnIndex];
 
         if (fileColumnIndex != -1) {
-            if (fileExpected(line) && !fileName.trim().equals("")) {
+            if (fileExpected(line) && !fileName.trim().equals(EMPTY)) {
                 if (!fileName.contains(".")) {
                     throw new FileFormatException(line[fileColumnIndex]); // Check that file has an extension
                 }
@@ -264,8 +273,8 @@ public final class MetadataSetter implements Callable<Integer> {
      * @param aRow A CSV row
      * @return True if a file is expected for this supplied row
      */
-    private boolean fileExpected(final String[] aRow) {
-        return !aRow[myCsvHeaders.getObjectTypeIndex()].equals("Collection");
+    private boolean fileExpected(final String... aRow) {
+        return !"Collection".equals(aRow[myCsvHeaders.getObjectTypeIndex()]);
     }
 
     /**
@@ -275,29 +284,30 @@ public final class MetadataSetter implements Callable<Integer> {
      * @throws FileNotFoundException If a media file could be found
      * @throws FfProbeException If FFProbe encounters an error while reading the media file
      */
+    @SuppressWarnings({ "PMD.CognitiveComplexity", "PMD.CyclomaticComplexity" })
     private void addMetadata(final String... aRow) {
-        final String filePath = aRow[myCsvHeaders.getFileNameIndex()];
+        final String csvSourceFile = aRow[myCsvHeaders.getFileNameIndex()];
 
         try {
+            final Path filePath = getFullFilePath(csvSourceFile);
             final FFprobe ffprobe = new FFprobe(myFfmpegPath);
-            final FFmpegProbeResult probeResult = ffprobe.probe(getFullFilePath(filePath));
+            final FFmpegProbeResult probeResult = ffprobe.probe(filePath.toString());
             final FFmpegFormat format = probeResult.getFormat();
-            final Path path = new File(filePath).toPath();
-            final String mimeType = Files.probeContentType(path);
+            final String mimeType = Files.probeContentType(filePath);
 
             if (mimeType.contains("audio") || mimeType.contains("video")) {
                 aRow[aRow.length - Constants.DURATION_OFFSET] = String.valueOf(format.duration);
 
                 if (myCsvHeaders.hasFormatExtentIndex() &&
-                    aRow[myCsvHeaders.getFormatExtentIndex()].trim().equals("")) {
+                        aRow[myCsvHeaders.getFormatExtentIndex()].trim().equals(EMPTY)) {
                     final double rawDuration = format.duration;
                     final int hours = (int) rawDuration / Constants.ONE_HOUR;
                     final int minutes = (int) (rawDuration % Constants.ONE_HOUR) / Constants.ONE_MINUTE;
                     final int seconds = (int) rawDuration % Constants.ONE_MINUTE;
-                    final String formattedHours = hours > 0 ? String.format("%02dh", hours) : "";
+                    final String formattedHours = hours > 0 ? String.format("%02dh", hours) : EMPTY;
                     final StringBuffer formattedDuration = new StringBuffer();
-                    formattedDuration.append(formattedHours);
-                    formattedDuration.append(String.format(" %02dm %02ds", minutes, seconds));
+
+                    formattedDuration.append(formattedHours).append(String.format(" %02dm %02ds", minutes, seconds));
                     aRow[myCsvHeaders.getFormatExtentIndex()] = formattedDuration.toString().trim();
                 }
 
@@ -308,6 +318,7 @@ public final class MetadataSetter implements Callable<Integer> {
                         if (stream.width != 0) {
                             aRow[aRow.length - Constants.WIDTH_OFFSET] = String.valueOf(stream.width);
                         }
+
                         if (stream.height != 0) {
                             aRow[aRow.length - Constants.HEIGHT_OFFSET] = String.valueOf(stream.height);
                         }
@@ -315,7 +326,7 @@ public final class MetadataSetter implements Callable<Integer> {
                 }
             }
         } catch (final IOException details) {
-            System.err.println(LOGGER.getMessage(MessageCodes.MG_106, filePath, details.getMessage()));
+            System.err.println(LOGGER.getMessage(MessageCodes.MG_106, csvSourceFile, details.getMessage()));
         }
     }
 
@@ -326,17 +337,17 @@ public final class MetadataSetter implements Callable<Integer> {
      * @return The full path to a file
      * @throws FileNotFoundException If the file could not be found at any of the possible paths
      */
-    private String getFullFilePath(final String aPartialPath) throws FileNotFoundException {
+    private Path getFullFilePath(final String aPartialPath) throws FileNotFoundException {
         for (final String mediaPath : myMediaPath) {
             final Path path = Path.of(mediaPath, aPartialPath);
 
             if (Files.exists(path)) {
-                return path.toString();
+                return path;
             }
         }
 
         throw new FileNotFoundException(LOGGER.getMessage(MessageCodes.MG_105, aPartialPath,
-            myMediaPath.contains(",") ? StringUtils.toString(',', myMediaPath) : myMediaPath));
+                myMediaPath.contains(",") ? StringUtils.toString(',', myMediaPath) : myMediaPath));
     }
 
     /**
